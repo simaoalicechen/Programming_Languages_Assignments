f7b4588b43126f1e5ef30cf18783203ca25285f0
Alice Chen <alicechen@Alices-MacBook-Pro.local>
2021-07-14 23:21:15 -0400

    Haskell Scheme projects

---
+++ Haskell_Assignment_2_Reverse_a_List/Reverse_a_List.hs
@@ -0,0 +1,11 @@
+--Write programs in Haskell that reverses a simple list of integers using recursion; specifically, in tail recursive form. Do not use higher order functions such as
+--“reverse”, comprehensive lists or functions from a library.
+
+reverse_list :: [int] -> [int]  
+reverse_list l = reverse_list_helper l []  
+    where
+        reverse_list_helper [] t = t
+        reverse_list_helper (x:xs) t = reverse_list_helper xs (x:t)
+
+main :: IO ()
+main = print (reverse_list [1,2,3,4,5,6,7,8,9,10])
